7316c43d53c4f6bcf20e729365f1f2b4332e872d
RushabhNarekar <46750784+RushabhNarekar@users.noreply.github.com>
2019-01-16 19:32:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,562 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<link rel="icon" href="images/icon.png">
+	<title>Welcome On Rushabh Narekar's Portfolio | Home :: Rushabh Narekar</title>
+
+	<!--meta tags -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="Intro Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--//meta tags ends here-->
+	<!--booststrap-->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+	<!--//booststrap end-->
+	<!-- font-awesome icons -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- //font-awesome icons -->
+<!-- Nav-CSS -->	
+<link href="css/nav.css" rel="stylesheet" type="text/css" media="all" />
+ <script src="js/modernizr.custom.js"></script>
+<!-- //Nav-CSS -->	
+	<link rel="stylesheet" href="css/lightbox.css">
+	<!--gallery-->
+
+	<!--stylesheets-->
+	<link href="css/style.css" rel='stylesheet' type='text/css' media="all">
+	<!--//stylesheets-->
+
+	<link href="//fonts.googleapis.com/css?family=Montserrat:300,400,500" rel="stylesheet">
+
+</head>
+<body>
+<!-- navigation section -->
+	<div class="header-outs">
+		<div class="header-w3layouts">
+		<div class="container">
+
+<div class="right-side"> 
+<p>
+		<button id="trigger-overlay" type="button">
+			<span class="fa fa-bars" aria-hidden="true"></span>
+		</button>
+		</p>
+</div>
+	<!-- open/close -->
+		<div class="overlay overlay-hugeinc">
+			<button type="button" class="overlay-close">Close</button>
+			<nav>
+				<ul>
+					<li><a href="#index.html" class="scroll">Home</a></li>
+					<li><a href="#about" class="scroll" >About</a></li>
+					<li><a href="#services" class="scroll" >Skills</a></li>
+					<li><a href="#experience" class="scroll" >Qualification</a></li>
+					<li><a href="#contact" class="scroll" >Contact</a></li>
+					
+				</ul>
+			</nav>
+		</div>
+					<div class="hedder-logo">
+				<h2><a href="index.html">Rushabh Narekar</a></h2>
+		</div>
+	<!-- /open/close -->
+<!-- /navigation section -->
+</div>
+<div class="clearfix"> </div>
+
+</div>
+<!-- Slideshow 4 -->
+		<div class="slider">
+			<div class="callbacks_container">
+				<ul class="rslides" id="slider4">
+					<li>
+						<div class="slider-img">
+							<div class="container">
+								<div class="slider-info">
+									<h4>I am Rushabh Narekar
+										</h4>
+									<p>Back-End & VB.Net Devloper</p>
+
+								</div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="slider-img ">
+							<div class="container">
+								<div class="slider-info">
+									<h4>I Am Back-End & VB.Net Devloper
+										 </h4>
+									<p>My Passion</p>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<!-- This is here just to demonstrate the callbacks -->
+		<!-- <ul class="events">
+      <li>Example 4 callback events</li>
+    </ul>-->
+</div>
+<!-- //banner -->
+<!--about -->
+	<div class="about" id="about">
+		<div class="container">
+		<div class="col-md-4 about-left ">
+					<img src="images/back.jpg" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-7 about-right">
+				<div class="about-me">
+				<h2>Rushabh Narekar</h2>
+				<h4>Back-end & VB.Net Developer</h4>
+				</div>
+				<div class="w3layouts_more-buttn">
+				<p>I Rushabh Narekar currently studies in Bachelor of Engineering in Computer engineering at Government Engineering of College Gandhinagar(sec28).<br><br>
+
+
+
+					My area of interest in IOT(Internet Of Things) , Python programming and also in Arduino and Raspberry PI circuits, Networking, Network security, Artificial Intelligence, Machine Learning, Programming languages PHP, Java, Android.
+				<div class="buttn-about">
+								<a href="#services">Read More</a>
+							</div>
+
+
+
+
+<div class="social-icons">
+
+			<ul>
+			<a href="https://www.facebook.com/narekar.rushabh" target="_blank">
+            <img src="images/icons/fb.png" title="Facebook" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="left" title="Tooltip on left"> </a> &nbsp
+            <a href="https://twitter.com/rush7549" target="_blank">
+            <img src="images/icons/twit.png" title="Twitter" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom"></a> &nbsp
+            <a href="https://github.com/rushabh219" target="_blank">
+            <img src="images/icons/git.png" title="Github" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom">
+            </a> &nbsp
+            <a href="https://www.instagram.com/rush551997/" target="_blank">
+            <img src="images/icons/insta.png" title="Instagram" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom"></a> &nbsp
+            <a href="https://www.linkedin.com/in/rushabh-narekar-738349156" target="_blank">
+            <img src="images/icons/link.png" title="Linkedin" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="right" title="Tooltip on right"></a> &nbsp
+            </ul>
+</div>
+
+
+
+
+							<div class="buttom-social-grids">
+
+					
+    </div>
+
+
+
+
+
+
+
+
+
+        </a> 
+
+				</div>
+							</div>
+							
+				</div>
+				<div class="clearfix"> </div>
+				<div class="my-ser-info">
+				<div class="col-md-5 my-datas">
+				<h3>My Personal Data</h3>
+				<ul class="address">
+										<li>
+							<ul class="address-text ">
+								<li><b>Name </b></li>
+								<li>:Rushabh Narekar</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="address-text ">
+								<li><b>D.O.B </b></li>
+								<li>:28-10-1997</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="address-text">
+								<li><b>PHONE </b></li>
+								<li>:+91 9722753135</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="address-text">
+								<li><b>ADDRESS </b></li>:22,Geetanagar Near Mery Park Socitey,Opp Kosa society,Sionnagar,Ahemdabad(380008) INDIA.
+							</ul>
+						</li>
+						<li>
+							<ul class="address-text">
+								<li><b>E-MAIL </b></li>
+								<li><a href="mailto:example@mail.com">: rush7665@gmail.com</a></li>
+							</ul>
+						</li>
+						<li>
+							<ul class="address-text">
+								<li><b>WEBSITE </b></li>
+								<li><a href="/#">: www.rushabhnarekar.com</a></li>
+							</ul>
+						</li>
+					</ul> 
+			<div class="hire-w3lgrids">
+						<a href="RESUME.pdf"  target=_"blank" class="there-more"> 
+							Resume				
+						</a> 
+						<a href="#" class="there-more"> 
+						Hire Me
+						</a>
+					</div>
+				</div>
+				<div class="col-md-7 aboutmy-services">
+								
+								<div class="col-md-6 col-sm-6 col-xs-6 about-ser-grid">
+								<div class="about-gap">
+								<span class="fa fa-television" aria-hidden="true"></span>
+								<h3>Web Designing</h3>
+								<p>Doing both programming and graphic design skills, specializing in usability and user experience.User experience and user pathway design.Front-end and back-end programming.Currently Working on live projects on recently based problems.
+									</p>
+								</div>
+								<div class="clearfix"> </div>
+								</div>
+								<div class="col-md-6 col-sm-6 col-xs-6 about-ser-grid">
+								<div class="about-gap clr-green">
+								<span class="fa fa-television" aria-hidden="true"></span>
+
+ 								<h3>VB.Net Devloper</h3>
+                          				 <p>Completing Project In VB.Net Work and complete the project of automatic fingerprint attendance system (8 months) project is based on IOT (Internet of Things) Technology and arduino intelligence circuit board. Software is based on VB.Net.
+
+</p>
+								</div>
+
+								</div>
+								<div class="col-md-6 col-sm-6 col-xs-6 about-ser-grid">
+								<div class="about-gap">
+								<span class="fa fa-television" aria-hidden="true"></span>
+								
+								<h3>Networking</h3>
+								<p>Managing and Maintaining the Servers, PC’s, Routers and Switches.Configuring, Managing and Maintaining Networking Equipments.Network processing, centralized and distributive network connection.Installing, configuring and administering network technologies. 
+</p>
+								</div>
+
+							
+
+								<div class="clearfix"> </div>
+								</div>
+								<div class="col-md-6 col-sm-6 col-xs-6 about-ser-grid">
+								<div class="about-gap clr-green">
+								<span class="fa fa-television" aria-hidden="true"></span>
+
+ 								<h3>Application Devloper</h3>
+                          				 <p>Developing an application totally based on Android platform dependent.Currently working on application based project which is solving the real time problem activities.
+</p>
+								</div>
+
+							</div>	
+							<div class="clearfix"> </div>
+							</div>
+							<div class="clearfix"> </div>
+
+</div>
+<br><br>
+<!--//about -->
+		<div class="services" id="services">
+		<div class="container">
+			<h3 class="title clr">Skills</h3>
+				<div class="clearfix"></div>
+			<div class="w3l_about_bottom_grid_right bar-grids">
+				<h6>Html<span> 70% </span></h6>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active" style="width: 70%">
+					</div>
+				</div>
+				<div class="mid-bar">
+					<h6>Android<span> 65% </span></h6>
+					<div class="progress">
+						<div class="progress-bar progress-bar-striped active" style="width: 65%">
+						</div>
+					</div>
+				</div>
+				<h6>Java<span>67% </span></h6>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active" style="width: 67%">
+					</div>
+
+				</div><br>
+	
+					<h6>Networking<span>75% </span></h6>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active" style="width: 75%">
+					</div>
+				
+			</div><br>
+			<h6>VB.Net<span>80% </span></h6>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active" style="width: 80%">
+					</div>
+				
+
+					
+</div>	
+
+			</div>
+		</div>
+
+		</div>
+				<!--// services-->
+<div class="experience" id="experience">
+	<div class="container">
+		<div class="education">
+		<div class="col-md-5 education-info">
+		 <h4>education</h4>
+		</div>
+		<div class="col-md-7 education-matter">
+		<div class="edu-right ">
+		<h6>2003-2015</h6>
+		<h5><b>Primary Education</b></h5>
+		<p><font color="black"><b>I completed my primary school era from Jay Somnath Primary School.</b></font></p>
+		</div>
+		<div class="edu-right back-shadow clr-green">
+				<h6>2014-2017</h6>
+		<h5><b>Diploma</b></h5>
+		<p><font color="black"><b>Completing diploma in computer engineering at R.C TECHNICHAL INSTITUTE.</b></font>	</p>
+		</div>
+		
+		<div class="edu-right ">
+				<h6>2017-2020</h6>
+		<h5><b>Bachelor Of Engineering</b></h5>
+		<p><font color="black"><b> Currently Purshuing Bachelor of Engineering In Computer engineering at Government Engineering Of College Gandhinagar(sec28).</b></font></p>
+		</div>
+		
+		</div>
+	
+		</div>
+		
+		</div>
+	</div>
+	
+<!-- gallery-->
+
+
+
+
+<!--contact-->
+	<div class="contact" id="contact" style="background: #adffe9;">
+		<div class="container" >
+			<h3 class="title">Contact</h3>
+						<div class="contact-us">
+				<form action="#" method="post">
+					<div class="col-md-6 col-sm-6 col-xs-6 styled-input">
+						<input type="text" name="name" placeholder="Name" required="">
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-6 styled-input">
+						<input type="text" name="last Name" placeholder="Last Name" required="">
+					</div>
+
+					<div class="col-md-6 col-sm-6 col-xs-6 styled-input">
+						<input type="email" name="email" placeholder="Email" required="">
+
+					</div>
+
+					<div class="col-md-6 col-sm-6 col-xs-6 styled-input">
+
+						<input type="text" name="phone" placeholder="phone" required="">
+
+					</div>
+					<div class="clearfix"> </div>
+					<div class="styled-input">
+
+						<textarea name="message" placeholder="Message" required=""></textarea>
+					</div>
+					<div>
+						<div class="click">
+							<input type="submit" value="SEND">
+						</div>
+					</div>
+				</form>
+			</div>
+			</div>
+           <section>
+           	<center>
+			<div class=" col-md-4 col-sm-4 col-xs-5 contact-icons ">
+				<div class="footer_grid_left" >
+					<b><h5>Address:</h5>
+					<p>22,Geetanagar Near Mery Park Socitey,Opp Kosa society,Sionnagar,Ahemdabad(380008) <span> Gujarat.
+						INDIA</span></p></b>
+				</div>
+				<b><div class="footer_grid_left">
+					<h5> Contact</h5>
+					<p>+91 9722753135</span></p>
+				</div></b>
+				<b><div class="footer_grid_left">
+					<h5>Email</h5>
+					<p><a href="mailto:info@example.com">rush7665@gmail.com</a>
+					<span><a href="mailto:info@example.com">rushnarekar@gmail.com</a></span></p>
+				</div></b>
+
+			</div>
+			<div class="clearfix"> </div>
+
+</center>
+</section>
+		
+		</div>
+	</div>
+
+<footer style="background: #adffe9;">
+<div class="social-icons">
+
+			<ul>
+			<a href="https://www.facebook.com/narekar.rushabh" target="_blank">
+            <img src="images/icons/fb.png" title="Facebook" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="left" title="Tooltip on left"> </a> &nbsp
+            <a href="https://twitter.com/rush7549" target="_blank">
+            <img src="images/icons/twit.png" title="Twitter" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom"></a> &nbsp
+            <a href="https://github.com/rushabh219" target="_blank">
+            <img src="images/icons/git.png" title="Github" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom">
+            </a> &nbsp
+            <a href="https://www.instagram.com/rush551997/" target="_blank">
+            <img src="images/icons/insta.png" title="Instagram" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom"></a> &nbsp
+            <a href="https://www.linkedin.com/in/rushabh-narekar-738349156" target="_blank">
+            <img src="images/icons/link.png" title="Linkedin" style="height: 35px; width: 35px;" data-toggle="tooltip" data-placement="right" title="Tooltip on right"></a> &nbsp
+            </ul>
+    </div>
+	</a> 
+</div>
+
+		<p><font color="black"><b> Copyright © 2018 Rushabh Narekar. Designed by Mr.Rushabh Narekar</b></font></p>
+	</footer>
+	<script src='js/jquery-2.2.3.min.js'></script>
+	<script src="js/bootstrap.js"></script>
+		<script src="js/classie.js"></script>
+		<script src="js/demo1.js"></script>
+	<script src="js/lightbox-plus-jquery.min.js"></script>
+
+	<script src="js/responsiveslides.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<!-- Jquery Core Js -->
+    <script src="js/tooltip/jquery.min.js"></script>
+
+
+    <!-- Bootstrap Core Js -->
+    <script src="js/tooltip/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="js/tooltip/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="js/tooltip/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="js/tooltip/waves.js"></script>
+
+    <!-- Custom Js -->
+    <script src="js/tooltip/admin.js"></script>
+    <script src="js/tooltip/tooltips-popovers.js"></script>
+
+    <!-- Demo Js -->
+    <script src="js/tooltip/demo.js"></script>
+
+
+
+
+
+
+
+
+	<script>
+		// You can also use "$(window).load(function() {"
+		$(function () {
+			// Slideshow 4
+			$("#slider4").responsiveSlides({
+				auto: true,
+				pager: false,
+				nav: true,
+				speed: 900,
+				namespace: "callbacks",
+				before: function () {
+					$('.events').append("<li>before event fired.</li>");
+				},
+				after: function () {
+					$('.events').append("<li>after event fired.</li>");
+				}
+			});
+
+		});
+	</script>
+	<!--// banner-->
+	<!-- OnScroll-Number-Increase-JavaScript -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+	<!-- //OnScroll-Number-Increase-JavaScript -->
+
+<!-- start-smoth-scrolling -->
+	<script src="js/move-top.js"></script>
+	<script src="js/easing.js"></script>
+	<script>
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 900);
+			});
+		});
+	</script>
+	<!-- start-smoth-scrolling -->
+	<!-- here stars scrolling icon -->
+	<script>
+		$(document).ready(function () {
+
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear'
+			};
+
+
+			$().UItoTop({
+				easingType: 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<!-- //here ends scrolling icon -->
+</body>
+
+</html>
+
+
